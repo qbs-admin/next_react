11f35fe044308da10cb28dec9225c1cbbcf58a90
qbsgokul <gokul@qbrainstorm.com>
2025-01-10 19:26:57 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+    output: 'export', // Enables static export
+    trailingSlash: true, // Adds trailing slashes for static hosting
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'http://localhost/my_app/' : '', // Set asset prefix only in production
+  };
+  
